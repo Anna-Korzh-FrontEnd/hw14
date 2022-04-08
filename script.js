b8645dfcecda56b5245e0a6eab5b552706cce6ed
kykyka <e.korj87@gmail.com>
2022-04-08 18:28:54 +0300

    fixes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 "use strict";
 
 //1:
-// import storage from './storage.js'
-// if (storage.get('users')){
-//     let users = storage.get('users');
-//     console.log(users)
-// } else {
-//     fetch("https://jsonplaceholder.typicode.com/posts/1")
-//         .then(response => response.json())
-//         .then((users) => storage.set("users",users))
-//         .catch((error) => console.error("Error"))
-// }
-// storage.remove("users")
+import storage from './storage.js'
+let post = storage.get('post');
+if (post){
+    console.log(post)
+} else {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then(response => response.json())
+        .then((post) => storage.set("post",post))
+        .catch((error) => console.error("Error"))
+}
+storage.remove("post")
 
- const isValidDateFormat = (str) => {
-        if(str.match(/^\d{4}\/\d{2}\/\d{2}\s\d{2}\:\d{2}$/)){
-                return true
-        } return false   
- }
+//  const isValidDateFormat = (str) => {
+//         if(str.match(/^\d{4}\/\d{2}\/\d{2}\s\d{2}\:\d{2}$/)){
+//                 return true
+//         } return false
+//  }
 
-console.log(isValidDateFormat("2012/09/18 12:10")); // true
-console.log(isValidDateFormat("12.02.2022 12:10")); // false
+// console.log(isValidDateFormat("2012/09/18 12:10")); // true
+// console.log(isValidDateFormat("12.02.2022 12:10")); // false
 
 
 
